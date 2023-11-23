e09a162df4c2616c34af1bac74d2e694c75dacc8
Sayan67 <sayan.das.21@aot.edu.in>
2023-11-23 18:48:56 +0530

    Added feature of not going in negative number in counter

--- counter(react-hooks)/counter/src/app/page.js
+++ counter(react-hooks)/counter/src/app/page.js
@@ -5,26 +5,39 @@ export default function Home() {
   let [counter, setCounter] = useState(0)
 
   let addValue = () => {
-    setCounter(counter+1)
+    if(!Number.isInteger(counter)){
+      setCounter(0)
+    }else{
+
+      setCounter(counter + 1)
+    }
   }
-  let removeValue =()=>{
-    setCounter(counter-1)
+  let removeValue = () => {
+    console.log(counter)
+    if (counter > 0) {
+      setCounter(counter-1)
+      //setTimeout(counter=0,2000)
+    }
+    else{
+
+      setCounter("No negative vibes!")
+    }
   }
   return (
     <>
       <div className="text-center items-center h-48">
         <div className="border-slate-400 m-auto w-1/2 border-2 h-48"><h1>Our value is: {counter}</h1></div>
-        <br/>
+        <br />
         <button
-        className="border-purple-400 border-2 px-3 py-1 rounded-md mb-2 bg-purple-500 hover:border-purple-800"
+          className="border-purple-400 border-2 px-3 py-1 rounded-md mb-2 bg-purple-500 hover:border-purple-800"
           onClick={addValue}
         >Add Value
         </button>
-        <br/>
+        <br />
         <button
-        className="border-purple-400 border-2 px-3 py-1 rounded-md bg-purple-500 hover:border-purple-800"
+          className="border-purple-400 border-2 px-3 py-1 rounded-md bg-purple-500 hover:border-purple-800"
           onClick={removeValue}
-        >Add Value
+        >Remove Value
         </button>
       </div>
 
